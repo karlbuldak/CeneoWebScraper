eb634dc07ea37e630bdea75df822b6b1b6e4dce2
Karol Bułdak <s221451@student.uek.krakow.pl>
2022-06-09 23:24:56 +0200

    9-06-2022 KB

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,3 +1,13 @@
 class Opinion:
-    def __init__(opnion_id, author, recommendation, stars, useful, useless, publish_date, purchase_date, pros, cons):
-        pass
+    def __init__(self, opnion_id, author, recommendation, stars, useful, useless, publish_date, purchase_date, pros, cons):
+        self.opinion_id = opinion_id
+        self.author = author
+        self.reccomendation = recommendation
+        self.stars = stars
+        self.content = content
+        self.useful = useful
+        self.useless = useless
+        self.publish_date = publish_date
+        self.purchase_date = purchase_date
+        self.pros = pros
+        self.cons = cons
--- app/models/product.py
+++ app/models/product.py
@@ -1,3 +1,4 @@
 class Product:
-    def __init__(product_id):
-        pass
+    def __init__(self, product_id, all_opinions):
+        self.product_id = product_id
+        self.all_opinions = all_opinions
